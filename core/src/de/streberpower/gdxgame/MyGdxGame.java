3c31555bc0028788e1b1b2192de13cc086308bc4
Otto Winter <kendama.otto@gmail.com>
2015-09-10 14:21:44 +0200

    Basic 3D Cube

--- core/src/de/streberpower/gdxgame/MyGdxGame.java
+++ core/src/de/streberpower/gdxgame/MyGdxGame.java
@@ -1,30 +1,55 @@
 package de.streberpower.gdxgame;
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 
-public class MyGdxGame extends Game {
-
-    public SpriteBatch batch;
-    public BitmapFont font;
+public class MyGdxGame extends ApplicationAdapter {
+    public Environment environment;
+    public PerspectiveCamera camera;
+    public ModelBatch modelBatch;
+    public Model model;
+    public ModelInstance instance;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        font = new BitmapFont();
-        this.setScreen(new MainMenuScreen(this));
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+        modelBatch = new ModelBatch();
+        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(10f, 10f, 10f);
+        camera.lookAt(0, 0, 0);
+        camera.near = 1f;
+        camera.far = 1000f;
+        camera.update();
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f, new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        instance = new ModelInstance(model);
     }
 
     @Override
     public void render() {
-        super.render();
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(camera);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        font.dispose();
+        modelBatch.dispose();
+        model.dispose();
     }
 }
