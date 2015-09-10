d64131d08b9280ce8c559940e72437ec7d9b616b
Otto Winter <kendama.otto@gmail.com>
2015-09-10 14:00:18 +0200

    Implement Basic 2D Game

--- core/src/de/streberpower/gdxgame/MyGdxGame.java
+++ core/src/de/streberpower/gdxgame/MyGdxGame.java
@@ -1,27 +1,30 @@
 package de.streberpower.gdxgame;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+public class MyGdxGame extends Game {
+
+    public SpriteBatch batch;
+    public BitmapFont font;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
 }
--- desktop/src/de/streberpower/gdxgame/desktop/DesktopLauncher.java
+++ desktop/src/de/streberpower/gdxgame/desktop/DesktopLauncher.java
@@ -7,9 +7,9 @@ import de.streberpower.gdxgame.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "GdxGame";
-        config.width = 480;
-        config.height = 320;
+        //config.title = "GdxGame";
+        //config.width = 480;
+        //config.height = 320;
         new LwjglApplication(new MyGdxGame(), config);
 	}
 }
